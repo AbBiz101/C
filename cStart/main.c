0a53a6c7b6998374a271d31bad3935b1d9a26787
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2023-01-12 12:18:09 +0100

    Update main.c

--- cStart/main.c
+++ cStart/main.c
@@ -1,12 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Student{
+     char name[20];
+     char major[20];
+     int age;
+     double gpa;
+};
 
 int main()
 {
-     char text1= 'A';
-     char text2[]= "lol";
-     int no= 3;
-     float no2= 23.9;
-     printf("%c %s %d %f", text1,text2,no,no2);
+     struct Student student1;
+     student1.age =20 ;
+     student1.gpa = 3,5;
+     strcpy(student1.name, "student");
+     strcpy(student1.major, "major");
+
+     struct Student student2;
+     student2.age =25 ;
+     student2.gpa = 4,5;
+     strcpy(student2.name, "student");
+     strcpy(student2.major, "major");
+     
+     printf("hallo");
      return 0;
 }
